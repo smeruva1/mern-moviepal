3509f4c40ab3ead66952ab3026f33f68f02be394
Sudar Meruva <meruva_s@yaho.com>
2020-05-14 17:19:53 -0400

    changed server.js to build

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -43,7 +43,8 @@ export const getMovieDetails = function (movieId) {
   //console.log("In API.js about to perform axios.get: " + movieId);
     return axios.get(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=US&source=tmdb&source_id=`+movieId,{headers: {
     "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-    "x-rapidapi-key": process.env.REACT_APP_UTELLY
+    //"x-rapidapi-key": process.env.REACT_APP_UTELLY
+    "x-rapidapi-key": "9e12b195f8msh211bda4699166cbp16fb57jsn1041353b0dde"
   }});        
 };
 
@@ -52,34 +53,34 @@ export const searchMovieByID = function (query) {
   //console.log(query);
   // return axios.get('https://api.themoviedb.org/3/search/movie?api_key=1fec72236532ee89a303c5cc707f12e4&query=' + query);
   // return axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + process.env.REACT_APP_THEMOVIEDB + '&query=' + query);
-  return axios.get('https://api.themoviedb.org/3/movie/'+query+'?api_key=' + process.env.REACT_APP_THEMOVIEDB +'&country=US&language=en-US');
+  return axios.get('https://api.themoviedb.org/3/movie/'+query+'?api_key=' + "1fec72236532ee89a303c5cc707f12e4" +'&country=US&language=en-US');
 }
 
 // make a search to The movie DB api
 export const searchTheMovies = function (query) {
   //console.log(query);
   // return axios.get('https://api.themoviedb.org/3/search/movie?api_key=1fec72236532ee89a303c5cc707f12e4&query=' + query);
-  return axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + process.env.REACT_APP_THEMOVIEDB + '&country=US&language=en-US&query=' + query);
+  return axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + "1fec72236532ee89a303c5cc707f12e4" + '&country=US&language=en-US&query=' + query);
 
 };
 
 //popular movies
 export const popularTheMovies = function () {
   //console.log("Hi from popular API axios call");
-  return axios.get('https://api.themoviedb.org/3/movie/popular?api_key=' + process.env.REACT_APP_THEMOVIEDB + '&country=US&language=en-US&page=1');
+  return axios.get('https://api.themoviedb.org/3/movie/popular?api_key=' + "1fec72236532ee89a303c5cc707f12e4" + '&country=US&language=en-US&page=1');
 };
 
 //new movies
 export const newTheMovies = function () {
-  return axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=' + process.env.REACT_APP_THEMOVIEDB + '&country=US&language=en-US&page=1');
+  return axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=' + "1fec72236532ee89a303c5cc707f12e4" + '&country=US&language=en-US&page=1');
 };
 
 //Top Rated movies
 export const topRatedTheMovies = function () {
-  return axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=' + process.env.REACT_APP_THEMOVIEDB + '&country=US&language=en-US&page=1');
+  return axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=' + "1fec72236532ee89a303c5cc707f12e4" + '&country=US&language=en-US&page=1');
 };
 
 //TV Shows 
 export const tvShowsTheMovies = function () {
-  return axios.get('https://api.themoviedb.org/3/tv/popular?api_key=' + process.env.REACT_APP_THEMOVIEDB + '&country=US&language=en-US&page=1');
+  return axios.get('https://api.themoviedb.org/3/tv/popular?api_key=' + "1fec72236532ee89a303c5cc707f12e4" + '&country=US&language=en-US&page=1');
 };
